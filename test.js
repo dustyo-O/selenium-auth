dc9ea8cc1bcb2a2eb4e7f34096e93ee20634d57a
Alexandr Shleyko <dusty@yandex-team.ru>
2016-11-06 15:17:29 +0300

    switch to selenium

--- test.js
+++ test.js
@@ -1,15 +1,33 @@
 var webdriverio = require('webdriverio');
 var options = {
     desiredCapabilities: {
-        browserName: 'firefox'
+        browserName: 'safari'
     }
 };
- 
 webdriverio
     .remote(options)
     .init()
-    .url('http://www.google.com')
-    .getTitle().then(function(title) {
-        console.log('Title was: ' + title);
-    })
-    .end();
+    .url('http://msk.npfdoverie.ru:9285/fo_crm/ru_RU/')
+    .waitForVisible('#userName', 5000)
+    .waitForVisible('#userPassword', 5000)
+    .setValue("#userName",'Бабенышева Анастасия Николаевна')
+    .click('#okButton');
+
+
+//client.findElement(By.id('userName')).sendKeys('Бабенышева Анастасия Николаевна');
+//userName.sendKeys('Бабенышева Анастасия Николаевна');
+
+/*client.get(URL)
+    .then(function() {
+        client.wait(until.elementLocated(By.id('userName')), 30000).then(
+            function()
+            {
+                client.findElement(By.id('userName')).sendKeys('Бабенышева Анастасия Николаевна');
+                client.findElement(By.id('userPassword')).sendKeys('NNU330V1');
+                client.findElement(By.id('okButton')).click();
+
+                client.quit();
+            }
+        );
+
+});*/
